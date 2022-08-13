829a4ebdf97da2f1b01c90eb1e2ba4bcb4dff243
Hebert Negron <hjnm194@gmail.com>
2022-08-13 11:45:26 -0500

    feat: added search contract by contractor name endpoint

--- routes/contracts.py
+++ routes/contracts.py
@@ -34,11 +34,19 @@ def create_contract(
     db.contracts.insert_one(contract_data)
     return responses.success_ok()
 
-
 @router.get("/", response_model=list[ContractOverview])
 def list_contracts(current_group=Depends(group_parameters), db: Database = Depends(get_db)):
     return retrieve_contracts(db, current_group)
 
+@router.get("/{query}", response_model=list[ContractOverview])
+def search_contract(query: str, db: Database = Depends(get_db), current_group=Depends(group_parameters)):
+    print(current_group)
+    return retrieve_contracts(db, {
+        '$and': [
+            { 'group_code': current_group},
+            { 'contractor_name': {'$regex': query} }
+        ]
+    })
 
 @router.get("/{id}", response_model=ContractDetails)
 def get_contract_details(id: PyObjectId, db: Database = Depends(get_db)):
